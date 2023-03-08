88f8db3112b9528c367b543721b3a896415705d7
Decio Neto <deciom.neto10@gmail.com>
2023-03-08 17:07:13 -0300

    stamp: 34

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+
+  // É válido também tornar o valor do selector '[app-servers]'...
+  // ... para então chamá-lo como <div app-servers />, por exemplo.
+
+  // Ou então como classe: '[.app-servers]'
+  // <div class=".app-servers" />
+
+  templateUrl: './servers.component.html',
+
+  // É possível também inserir <app-server> no templateUrl e ele irá renderizar o componente diretamente.
+  // Assim como é aceito string interpolation (``).
+
+  styleUrls: ['./servers.component.css'],
+
+  // Pode-se inserir outros documentos no array acima...
+
+  // ... ou então declarar diretamente o css como mostrado abaixo.
+  // styles: [`h3 {color: darkBlue}`]
+})
+export class ServersComponent {
+  allowNewServer = false
+  serverCreationStatus = 'No server was created.'
+  serverName = 'Server name here'
+
+  createServer() {
+    this.serverCreationStatus = `A new server has been created with the name '${ this.serverName }'`
+  }
+
+  updateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value
+  }
+
+  constructor() {
+    setTimeout(() => this.allowNewServer = true, 2000)
+  }
+}
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './components/**/*.{html,js}',
+    './pages/**/*.{html,js}',
+    './index.html',
+    './public/index.html',
+    './src/**/*.{html,js}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
