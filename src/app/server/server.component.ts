64f7cd29b9b203c90d940a2cbe37c6f4e178f706
Decio Neto <deciom.neto10@gmail.com>
2023-03-08 17:34:30 -0300

    stamp: 39

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -7,7 +7,17 @@ import { Component } from '@angular/core';
 })
 export class ServerComponent {
   serverId = 10;
-  serverStatus = 'offline';
+  serverStatus = 'offline'
+  statusColor = 'red'
+
+  constructor() {
+    const num = Math.random()
+
+    if (num > 0.5) {
+      this.serverStatus = 'online'
+      this.statusColor = 'green'
+    }
+  }
 
   getServerStatus() {
     return this.serverStatus;
