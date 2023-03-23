3ad06db45afb7254fc07b6cd9ee5376955b60ff4
Decio Neto <deciom.neto10@gmail.com>
2023-03-23 16:30:15 -0300

    fea: create users component

---
+++ src/app/components/users/users.component.ts
@@ -0,0 +1,29 @@
+import { Component } from "@angular/core"
+import { ActivatedRoute, Router } from "@angular/router"
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html'
+})
+export class UsersComponent {
+  users = [
+    { id: 1, name: 'Foo' },
+    { id: 2, name: 'Bar' },
+    { id: 3, name: 'Viz' }
+  ]
+
+  constructor(private router: Router, private route: ActivatedRoute) { }
+
+  onClick() {
+    this.router.navigate(['/servers'])
+  }
+
+  reloadPage() {
+    // Angular verifica que eu já estou na rota 'routes', por isso ele não recarrega a rota.
+    this.router.navigate(['users'])
+  }
+
+  brokenReloadPage() {
+    this.router.navigate(['users'], { relativeTo: this.route })
+  }
+}
