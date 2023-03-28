8e85b004336c88db4c1f8071b2a3b76b8ad3d332
Decio Neto <deciom.neto10@gmail.com>
2023-03-28 18:44:15 -0300

    refactor: create theme color classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,14 @@ module.exports = {
     './src/**/*.{html,js}'
   ],
   theme: {
-    extend: {}
-  },
-  plugins: []
+    extend: {
+      colors: {
+        primary: 'rgb(59 130 246)',
+        secondary: 'rgb(107 114 128)',
+        success: 'rgb(34 197 94)',
+        warning: 'rgb(234 179 8)',
+        danger: 'rgb(239 68 68)'
+      }
+    }
+  }
 }
