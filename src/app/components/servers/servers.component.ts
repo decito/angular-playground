811ee2a84c51d6333a18111e097115c4f778a956
Decio Neto <deciom.neto10@gmail.com>
2023-03-29 18:56:00 -0300

    fix: integrate new routes behaviour to servers components

--- src/app/components/servers/servers.component.ts
+++ src/app/components/servers/servers.component.ts
@@ -6,41 +6,11 @@ import type { Server } from 'src/app/types/server'
 
 @Component({
   selector: 'app-servers',
-
-  /* É válido também tornar o valor do selector igual a '[app-servers]',
-   * tornando-o uma diretiva.
-   * Para usar no formato de diretiva, pode-se usar <div app-servers />
-   * em um outro componente, por exemplo.
-   *
-   * Ou então tornar seu valor com sendo '[.app-servers]'.
-   * Ao fazer isso, o selector ganha valor de classe,
-   * podendo ser chamado assim, por exemplo: <div class=".app-servers" />
-   */
-
   templateUrl: './servers.component.html'
-
-  /* É possível também informar diretamente o valor do template
-   * utilizando a propriedade template. Neste caso, um exemplo seria:
-   * template: '<app-server></app-server><app-server></app-server>'.
-   * É possível informar utilizando string interpolation (`) neste caso.
-   */
-
-  /**/
-
-  /* Para estilizar, tem duas formas:
-   * a primeira é utilizando o caminho para a folha de estilo
-   * com a propriedade styleUrls.
-   * Ex: ['./servers.component.css'],
-   *
-   * a segunda é informando diretamente o css inline
-   * com a propriedade styles.
-   * Ex: styles: [`h3 {color: darkBlue}`]
-   */
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false
-  // serverCreationStatus = 'No server was created.'
-  newServerName = 'Test'
+  newServerName = 'New Server'
   newServerCreated = false
 
   serverList: Server[]
