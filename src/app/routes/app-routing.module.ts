cec1add0510ddb5513bbcccf8913aa9b78a0b64f
Decio Neto <deciom.neto10@gmail.com>
2023-03-31 18:51:39 -0300

    refactor: change app-routing imports path

--- src/app/routes/app-routing.module.ts
+++ src/app/routes/app-routing.module.ts
@@ -1,20 +1,20 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 
-import { AuthGuard } from './guards/auth-guard.service'
-import { CanDeativateGuard } from './guards/can-deactivate-guard.service'
+import { AuthGuard } from '~/routes/guards/auth-guard.service'
+import { CanDeativateGuard } from '~/routes/guards/can-deactivate-guard.service'
 
-import { AccountsComponent } from '../components/accounts/accounts.component'
-import { AssignmentTwoComponent } from '../components/assignments/assignment-2/assignment-2.component'
-import { AssignmentThreeComponent } from '../components/assignments/assignment-3/assignment-3.component'
-import { AssignmentFourComponent } from '../components/assignments/assignment-4/assignment-4.component'
-import { AssignmentFiveComponent } from '../components/assignments/assignment-5/assignment-5.component'
-import { AssignmentsComponent } from '../components/assignments/assignments.component'
-import { PageNotFoundComponent } from '../components/page-not-found/page-not-found.component'
-import { ServerEditComponent } from '../components/servers/server/server-edit/server-edit.component'
-import { ServersComponent } from '../components/servers/servers.component'
-import { UserComponent } from '../components/users/user/user.component'
-import { UsersComponent } from '../components/users/users.component'
+import { AccountsComponent } from '~/components/accounts/accounts.component'
+import { AssignmentTwoComponent } from '~/components/assignments/assignment-2/assignment-2.component'
+import { AssignmentThreeComponent } from '~/components/assignments/assignment-3/assignment-3.component'
+import { AssignmentFourComponent } from '~/components/assignments/assignment-4/assignment-4.component'
+import { AssignmentFiveComponent } from '~/components/assignments/assignment-5/assignment-5.component'
+import { AssignmentsComponent } from '~/components/assignments/assignments.component'
+import { PageNotFoundComponent } from '~/components/page-not-found/page-not-found.component'
+import { ServerEditComponent } from '~/components/servers/server/server-edit/server-edit.component'
+import { ServersComponent } from '~/components/servers/servers.component'
+import { UserComponent } from '~/components/users/user/user.component'
+import { UsersComponent } from '~/components/users/users.component'
 
 export const appRoutes: Routes = [
   { path: '', redirectTo: '/servers', pathMatch: 'full' },
