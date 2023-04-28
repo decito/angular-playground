447ca5b0f5a18bd49f2826304dd85ae7f1b7b951
decito <deciom.neto10@gmail.com>
2023-04-28 16:59:21 -0300

    chore: remove unused import in assignment 7

--- src/app/components/assignments/assignment-7/assignment-7.component.ts
+++ src/app/components/assignments/assignment-7/assignment-7.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core'
-import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms'
+import { FormControl, FormGroup, Validators } from '@angular/forms'
 import { ActivatedRoute } from '@angular/router'
 import { Observable } from 'rxjs/internal/Observable'
 
