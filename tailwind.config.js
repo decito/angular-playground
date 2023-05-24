96a58f0bb3d7a56b6afc163ca0e92431db3bb3f6
decito <deciom.neto10@gmail.com>
2023-05-24 10:52:08 -0300

    refactor: add tailwind variables and remove unused safelist

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,43 +10,13 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: 'rgb(59 130 246)',
-        secondary: 'rgb(209 213 219)',
-        success: 'rgb(34 197 94)',
-        warning: 'rgb(234 179 8)',
-        danger: 'rgb(239 68 68)',
-        disabled: 'rgb(107 114 128)'
+        primary: 'rgb(var(--color-primary) / <alpha-value>)',
+        secondary: 'rgb(var(--color-secondary) / <alpha-value>)',
+        success: 'rgb(var(--color-success) / <alpha-value>)',
+        warning: 'rgb(var(--color-warning) / <alpha-value>)',
+        danger: 'rgb(var(--color-danger) / <alpha-value>)',
+        disabled: 'rgb(var(--color-disabled) / <alpha-value>)'
       }
     }
-  },
-  safelist: [
-    'p-1',
-    'p-2',
-    'p-3',
-    'bg-primary',
-    'border-primary',
-    'text-primary',
-    'bg-secondary',
-    'border-secondary',
-    'text-secondary',
-    'bg-success',
-    'border-success',
-    'text-success',
-    'bg-warning',
-    'border-warning',
-    'text-warning',
-    'bg-danger',
-    'border-danger',
-    'text-danger',
-    'bg-disabled',
-    'border-disabled',
-    'text-disabled',
-    'hover:brightness-110',
-    'hover:text-white',
-    'hover:bg-primary',
-    'hover:bg-secondary',
-    'hover:bg-success',
-    'hover:bg-warning',
-    'hover:bg-danger'
-  ]
+  }
 }
