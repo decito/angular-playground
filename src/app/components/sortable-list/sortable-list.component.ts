cfa7630017ae8360434f6a567de611357bc590c0
Decio Neto <deciom.neto10@gmail.com>
2023-06-13 11:57:08 -0300

    revert: sortable list component to ground zero

--- src/app/components/sortable-list/sortable-list.component.ts
+++ src/app/components/sortable-list/sortable-list.component.ts
@@ -6,8 +6,9 @@ import { Component } from '@angular/core'
 })
 export class SortableListComponent {
   activeUsers: string[] = ['Fulano', 'Sicrano', 'Beltrano']
-
   inactiveUsers: string[] = ['João', 'Maria', 'Zé da Manga']
+
+  draggingIndex: number
 
   setActive(index: number): void {
     const user = this.inactiveUsers.splice(index, 1)[0]
@@ -17,5 +18,50 @@ export class SortableListComponent {
   setInactive(index: number): void {
     const user = this.activeUsers.splice(index, 1)[0]
     this.inactiveUsers.push(user)
+  }
+
+  onDragStart(event: DragEvent, index: number): void {
+    // disparado quando o <li> é agarrado
+    // disparado uma vez
+    this.draggingIndex = index
+
+    console.log('dragstart')
+  }
+
+  onDragOver(event: DragEvent): void {
+    // disparado toda vez que o <li> passa por cima da <section>
+    // disparado varias vezes
+    event.preventDefault()
+
+    console.log('dragover')
+  }
+
+  onDragEnter(event: DragEvent): void {
+    // disparado quando o <li> entra na <section>
+    // disparado uma vez
+    event.preventDefault()
+
+    console.log('dragenter')
+  }
+
+  onDrop(event: DragEvent): void {
+    // disparado quando o <li> é solto dentro da <section>
+    // disparado uma vez
+    event.preventDefault()
+
+    const el = event.target as Element
+
+    const item = el.textContent
+
+    // verifica se o item existe na lista. isso evita criar um <li> fantasma
+    if (this.inactiveUsers.some(i => i === item)) console.log('achei')
+  }
+
+  onDragEnd(event: DragEvent) {
+    // disparado quando solta o <li>
+    // disparado uma vez
+    event.preventDefault()
+
+    console.log('dragend')
   }
 }
