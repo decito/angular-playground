55ad7b1b043a7ded43cad3d298e527d756dc3201
decito <deciom.neto10@gmail.com>
2023-07-28 14:11:48 -0300

    feat: add buttons to not found page

--- src/app/components/error-page/error-page.component.ts
+++ src/app/components/error-page/error-page.component.ts
@@ -1,3 +1,4 @@
+import { Location } from '@angular/common'
 import { Component, OnInit } from '@angular/core'
 import { ActivatedRoute, Data } from '@angular/router'
 
@@ -8,11 +9,15 @@ import { ActivatedRoute, Data } from '@angular/router'
 export class ErrorPageComponent implements OnInit {
   errorMessage: string
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private location: Location) {}
 
   ngOnInit() {
     // this.errorMessage = this.route.snapshot.data['message']
 
     this.route.data.subscribe((d: Data) => (this.errorMessage = d['message']))
+  }
+
+  goBack() {
+    this.location.back()
   }
 }
